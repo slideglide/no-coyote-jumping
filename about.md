8f6c5772a0efb7fe98bfdd2ef276dd1bce062b2b
slideglide <hi@example.com>
2026-06-30 21:48:58 +0300

    fix mod metadata + add warning

--- about.md
+++ about.md
@@ -1,5 +1,5 @@
-# Platformer No Coyote Jumping
+# No Coyote Jumping
 Ever wanted to make your platformer experience more miserable than it already is? Here's a mod that does just that! 
 This mod disables coyote jumping in platformer, making your platformer experience even more miserable.
 
-This mod will most likely break macro bots, so if you are a bot developer, I have added a soft toggle to the mod settings for you to programmatically disable.
+This mod will most likely break replay bots, so if you are a bot developer, I have added a soft toggle to the mod settings for you to programmatically disable.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,76 @@
 #include <Geode/loader/Mod.hpp>
+#include <Geode/modify/EditLevelLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/LevelPage.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/LevelAreaInnerLayer.hpp>
 
 using namespace geode::prelude;
 
+// all of the warning logic here
+static bool activeFor(GJGameLevel* level) {
+    return Mod::get()->getSettingValue<bool>("enabled") && level && level->isPlatformer();
+}
+
+static bool showWarnFor(GJGameLevel* level) {
+    if (!activeFor(level)) return false;
+
+    createQuickPopup(
+        "Notice",
+        "The <cl>No Coyote Jumping</c> mod is enabled. It is recommended that you <cr>disable the mod</c> if you intend to make <cy>real progress</c> on the level.",
+        "Close", "Disable Mod",
+        [](auto, bool btn2) {
+            if (btn2) {
+                Mod::get()->setSettingValue<bool>("enabled", false);
+            }
+        }
+    );
+
+    return true;
+}
+
+#define WARN_PLATFORMER_HOOK(name, cls)            \
+class $modify(name, cls) {                         \
+    struct Fields {                                \
+        bool shownWarn = false;                    \
+    };                                             \
+                                                   \
+    void onPlay(CCObject* sender) {                \
+        if (!m_fields->shownWarn && showWarnFor(m_level)) { \
+            m_fields->shownWarn = true;            \
+            return;                                \
+        }                                          \
+        cls::onPlay(sender);                       \
+    }                                              \
+};
+
+WARN_PLATFORMER_HOOK(NoCoyoteLevelInfoLayer, LevelInfoLayer)
+WARN_PLATFORMER_HOOK(NoCoyoteLevelPage, LevelPage)
+WARN_PLATFORMER_HOOK(NoCoyoteEditLevelLayer, EditLevelLayer)
+
+    // the tower warning
+class $modify(NoCoyoteLevelAreaInnerLayer, LevelAreaInnerLayer) {
+    struct Fields {
+        bool shownWarn = false;
+    };
+
+    void onDoor(CCObject* sender) {
+        auto levelID = sender->getTag();
+        auto level = GameLevelManager::get()->getMainLevel(levelID, true);
+
+        auto fields = m_fields.self();
+
+        if (!fields->shownWarn && showWarnFor(level)) {
+            fields->shownWarn = true;
+            return;
+        }
+
+        LevelAreaInnerLayer::onDoor(sender);
+    }
+};
+
+// the actual coyote jump disabling logic
 class $modify(NoCoyotePlayerObject, PlayerObject) {
     struct Fields {
         bool landedThisCollision = false;
